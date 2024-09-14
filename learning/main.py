808ae69b22102587d66a67d47afe6764ad5ca955
Henok Teixeira <henokteixeira@gmail.com>
2024-09-14 18:21:22 -0300

    feat: add basic text detection test in images with easyocr

---
+++ learning/main.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+import easyocr
+
+reader = easyocr.Reader(['pt'], gpu=True)
+img = cv.imread(cv.samples.findFile('../docs/mark-books.png'))
+
+text_detected = reader.readtext(img)
+
+for bbox, text, score in text_detected:
+    if score > 0.65:
+        cv.rectangle(img, tuple(map(int, bbox[0])), tuple(map(int, bbox[2])), (0, 255, 0), 1)
+
+cv.imshow('Image', img)
+cv.waitKey(0)
